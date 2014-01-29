ea4281f5542b2877f0e3bcdee069228d21f1209d
Jerrold Thompson <jet@whidbey.com>
2014-01-29 13:22:36 -0800

    Auth from Scratch

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.16'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  # gem 'therubyracer', :platforms => :ruby
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+group :test do
+  gem 'minitest-spec-rails', require: false
+  gem 'minitest-rails-capybara'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '329ec2d043826e59adb7c2e11c862384cf769f31834d935f766c02023fc977bfbd8e03c83510453120aaaf05f08a8097013d57535246bb456eb7557f8d9ca5ff'
